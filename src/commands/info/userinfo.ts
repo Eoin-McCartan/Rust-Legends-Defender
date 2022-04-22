1546e5e78132bb3e6f49bfee8ce37699cf61fbfd
@Strobez <41481352+Strrobez@users.noreply.github.com>
2022-04-22 23:28:40 +0100

    Userinfo Command

--- src/commands/info/userinfo.ts
+++ src/commands/info/userinfo.ts
@@ -1,4 +1,5 @@
-import { MessageEmbed } from "discord.js";
+import { GuildMember, MessageEmbed, User } from "discord.js";
+import { client } from "../..";
 import { Command } from "../../structures/Command";
 
 const status_emojis = {
@@ -11,7 +12,7 @@ const status_emojis = {
 export default new Command({
     name: "userinfo",
     description: "Display information about a user.",
-    userPermissions: ["BAN_MEMBERS"],
+    userPermissions: ["MANAGE_MESSAGES"],
     options: [
         {
             name: "target",
@@ -22,33 +23,34 @@ export default new Command({
     ],
     run: async ({ interaction }) =>
     {
-        let target = interaction.options.getUser("target");
+        let target: User = interaction.options.getUser("target");
 
-        let target_member = await interaction.guild.members.fetch(target.id);
+        let target_member: GuildMember = await interaction.guild.members.fetch(target.id);
 
         if (!target_member)
         {
-            return interaction.channel.send(`🚫 Couln't find ${target.username} in the server.`);
+            return interaction.channel.send(`❌ Couln't find ${target} in the server.`);
         }
 
-        let member_number = interaction.guild.members.cache.filter(m => m.joinedTimestamp > target_member.joinedTimestamp).size;    
-        let member_roles = target_member.roles.cache.filter(r => r.id !== interaction.guild.id).map(x => `\`${x.name}\``).join(", ")
-        let member_status = target_member.presence?.status 
-            ? `${status_emojis[target_member.presence.status]} **${target_member.presence.status}**`
-            : "⚫ **Invisible**";
+        let member_number: number   = interaction.guild.members.cache.filter(m => m.joinedTimestamp > target_member.joinedTimestamp).size;    
+        
+        let member_roles: string    = target_member.roles.cache.filter(r => r.id !== interaction.guild.id).map(x => `\`${x.name}\``).join(", ");
+
+        let member_status: string   = target_member.presence?.status
+            ? `${status_emojis[target_member.presence?.status]} **${target_member.presence?.status}**`
+            : "Unknown";
 
         let embed: MessageEmbed = new MessageEmbed();
             embed.setColor(target_member.roles.highest.color ?? "#000000");
             embed.setThumbnail(target.avatarURL());
-
             embed.setDescription(
-                `▫ Discord ID: **${target.id}**\n` + 
-                `▫ Roles: **${member_roles}**\n` +
-                `▫ Status: ${member_status}\n` +
-                `▫ Account Creation: **${new Date(target.createdAt).toUTCString()}**\n` +
-                `▫ Guild Join Date: **${new Date(target_member.joinedAt).toUTCString()}** ${member_number > 0 ? `\`(#${member_number})\`` : ""}`
+                `▫ **Discord ID:** ${target.id}\n` + 
+                `▫ **Roles:** ${member_roles}\n` +
+                `▫ **Status:** ${member_status}\n` +
+                `▫ **Account Creation:** ${new Date(target.createdAt).toUTCString()}\n` +
+                `▫ **Guild Join Date:** ${new Date(target_member.joinedAt).toUTCString()} ${member_number > 0 ? `\`(#${member_number})\`` : ""}`
             );
-        
+
         return interaction.followUp({ 
             content: `👤 Information about ${target.tag}:`, 
             embeds: [embed]
